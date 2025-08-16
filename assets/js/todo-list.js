0aff2c7fce478823b383424fb8b322584d2bb73c
nahuel <nahuel.ivaan@gmail.con>
2025-08-16 19:43:02 -0300

    Estilos modificados, funcion eliminar tarea y capitalizar tareas

---
+++ assets/js/todo-list.js
@@ -0,0 +1,52 @@
+const formList = document.querySelector("form");
+formList.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const tareaData = new FormData(formList);
+  const tareaDescription = tareaData.get("tarea");
+
+  const tareaAnalizada = validarTarea(tareaDescription);
+  const tareaCapitalizada = capitalizarText(tareaAnalizada);
+
+  const unorderList = document.querySelector(".listaTareas");
+  if (tareaCapitalizada) {
+    const createLi = document.createElement("li");
+    event.target.reset();
+    createLi.innerHTML = `
+      <div class="nuevaTarea">  
+        <input type="checkbox">
+        <p>${tareaCapitalizada}</p>
+        <button class="eliminarTarea">Eliminar</button>
+      </div>
+        `;
+    unorderList.prepend(createLi);
+  }
+
+  const bottonEliminar = document.querySelectorAll(".eliminarTarea");
+
+  bottonEliminar.forEach((button) => {
+    button.addEventListener("click", () => {
+      const containerBotton = button.parentElement;
+      containerBotton.remove();
+    });
+  });
+});
+
+function validarTarea(tarea) {
+  if (tarea.length > 3 && tarea.length <= 25) {
+    return tarea;
+  } else {
+    return false;
+  }
+}
+
+function capitalizarText(text) {
+  if(text && text.length > 0) {
+    return text[0].toUpperCase() + text.substring(1);
+  }
+  return '';
+}
+
+// ESTILAR LI
+// FUNCION ELIMINAR TAREA
+// CHECKBOX TACHA TAREA
